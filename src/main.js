5b2e8551ebaf90f17390ab3d69e4d2b6ff9b5398
JeanNewman <newmanrios@gmail.com>
2020-11-24 04:07:10 -0500

    first commit
    toda la pagina todavia me falta terminar el footer

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
+import './assets/css/style.css'
+import './assets/css/boostrap.min.css'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faUserSecret)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+Vue.config.productionTip = false
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+    render: h => h(App),
 }).$mount('#app')
