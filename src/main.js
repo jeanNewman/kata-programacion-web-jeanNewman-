2bc0667c9a967ee3d22504884fe1aca6e4fa2513
JeanNewman <newmanrios@gmail.com>
2020-11-25 23:11:45 -0500

    varios cambios

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
-import './assets/css/style.css'
+
 import './assets/css/boostrap.min.css'
+import './assets/css/style.css'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
